14c002721a929f664f4d738aca1d2f8d9c728f2f
akd <akd@ns-mac-2.local>
2017-05-17 16:41:05 +0300

    Add qsort @wip

--- sort.rb
+++ sort.rb
@@ -8,6 +8,20 @@ class Sort
       break if inherit_index == 0
     end
     index += 1
-    sort(arr, index)
+    insertion_sort(arr, index)
+  end
+
+  def self.qsort(arr)
+    return arr if arr.length <= 1
+    element = arr[0]
+    hash = arr.group_by { |x| x < element }
+    small  = hash[true] || []
+    big = hash[false] || []
+    big.delete(element)
+    qsort(small) + [element] + qsort(big)
   end
 end
+
+ARR = [5, 10, -1, 121, 13, 1, 7]
+
+puts Sort.qsort(ARR)
